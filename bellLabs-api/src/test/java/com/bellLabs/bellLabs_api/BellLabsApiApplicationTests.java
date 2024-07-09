ae30f87b03f50eff41bc0c81e558958576952874
abcamuez <82919641+abcamuez@users.noreply.github.com>
2024-07-09 12:01:59 -0400

    added spring api

---
+++ bellLabs-api/src/test/java/com/bellLabs/bellLabs_api/BellLabsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellLabs.bellLabs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellLabsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
