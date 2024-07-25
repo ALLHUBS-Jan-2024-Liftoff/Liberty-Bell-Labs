c607594fddf3c5523fe909d0dbf311496477fd96
Anthony Singiser <asingiser@gmail.com>
2024-07-25 11:45:46 -0400

    add routing with react-router-dom to app.jsx

--- bellLabs-ui/src/App.jsx
+++ bellLabs-ui/src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   
 
   return (
-    <div className="App">
-
-      <NavBar/>
-      
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <Routes>
+          <Route 
+            path="/dashboard" 
+            element={<Dashboard/>}
+          />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
