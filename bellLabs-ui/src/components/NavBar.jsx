8fbe688c15d381cd710a6e51814e47cf733e253c
abcamuez <82919641+abcamuez@users.noreply.github.com>
2024-07-25 14:54:27 -0400

    added signup link and login link to navbar

--- bellLabs-ui/src/components/NavBar.jsx
+++ bellLabs-ui/src/components/NavBar.jsx
@@ -14,8 +14,19 @@ export default function NavBar() {
         <a className="nav-link active" aria-current="page" href="#">Home</a>
         <a className="nav-link" href="#">Features</a>
         <a className="nav-link" href="#">Pricing</a>
-        {/* <a className="nav-link disabled" aria-disabled="true">Disabled</a> */}
+        
       </div>
+        {/* right side nav bar sign up and login */}
+
+        <ul class="nav navbar-nav ms-auto">
+        <li className="nav-item">
+          <a className="nav-link ms-3" href="#"><span className="glyphicon glyphicon-user"></span>Sign up</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link ms-3" href="#"><span className="glyphicon glyphicon-user"></span>Login</a>
+        </li>
+        </ul>
     </div>
   </div>
 </nav>
