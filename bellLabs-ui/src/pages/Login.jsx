4a1838e9bfdbd18d6fff6eef1d24bc24792639ec
Oluwasegun <kto32@drexel.edu>
2024-08-12 23:57:20 -0400

    Added the Logout page

--- bellLabs-ui/src/pages/Login.jsx
+++ bellLabs-ui/src/pages/Login.jsx
@@ -1,68 +1,68 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-// function Login({ setAuthenticated }) {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [message, setMessage] = useState("");
-//   const navigate = useNavigate();
+function Login({ setAuthenticated }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-//   const handleLogin = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post(
-//         "http://localhost:8080/login",
-//         {
-//           username,
-//           password,
-//         },
-//         {
-//           withCredentials: true,
-//         }
-//       );
-//       setAuthenticated(true);
-//       setMessage(response.data.message);
-//       navigate("/dashboard");
-//     } catch (error) {
-//       setMessage(error.response?.data?.message || "Login failed");
-//     }
-//   };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/login",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      setAuthenticated(true);
+      setMessage(response.data.message);
+      navigate("/dashboard");
+    } catch (error) {
+      setMessage(error.response?.data?.message || "Login failed");
+    }
+  };
 
-//   return (
-//     <div className="container mt-5">
-//       <h2>Login</h2>
-//       <form onSubmit={handleLogin}>
-//       <div className="mb-3">
-//           <label className="form-label">Username</label>
-//           <input
-//             type="text"
-//             className="form-control"
-//             value={username}
-//             onChange={(e) => setUsername(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <div className="mb-3">
-//           <label className="form-label">Password</label>
-//           <input
-//             type="password"
-//             className="form-control"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <button type="submit" className="btn btn-primary">
-//           Login
-//         </button> 
-//         <div className="mb-3">
-//             <p>Don't have an account? <a href="/register">Create one.</a></p>
-//         </div>
-//       </form>
-//       {message && <p>{message}</p>}
-//     </div>
-//   );
-// }
+  return (
+    <div className="container mt-5">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+      <div className="mb-3">
+          <label className="form-label">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button> 
+        <div className="mb-3">
+            <p>Don't have an account? <a href="/register">Create one.</a></p>
+        </div>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
 
-// export default Login;
+export default Login;
--- bellLabs-ui/src/pages/Logout.jsx
+++ bellLabs-ui/src/pages/Logout.jsx
@@ -1,20 +1,20 @@
-// import React from "react";
-// import axios from "axios";
+import React from "react";
+import axios from "axios";
 
-// function Logout({ setAuthenticated }) {
-//   const handleLogout = async () => {
-//     try {
-//       await axios.get("http://localhost:8080/logout",
-//         {
-//           withCredentials: true,
-//         });
-//       setAuthenticated(false);
-//     } catch (error) {
-//       console.error("Logout failed");
-//     }
-//   };
+function Logout({ setAuthenticated }) {
+  const handleLogout = async () => {
+    try {
+      await axios.get("http://localhost:8080/logout",
+        {
+          withCredentials: true,
+        });
+      setAuthenticated(false);
+    } catch (error) {
+      console.error("Logout failed");
+    }
+  };
 
-//   return <button onClick={handleLogout}>Logout</button>;
-// }
+  return <button onClick={handleLogout}>Logout</button>;
+}
 
-// export default Logout;
+export default Logout;
